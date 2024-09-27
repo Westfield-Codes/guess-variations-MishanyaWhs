bdf5973c640982e0e769ac0d5503fd98298ebcd7
MishanyaWhs <Mykhailo208202@student-schoolsofwestfield.org>
2024-09-27 15:09:53 +0000

    0

--- done.js
+++ done.js
@@ -1,1 +1,8 @@
-/* Put done code here */
+/* Define a function called rollDice */
+function rollDice(){
+    // create a variable called roll, set it equal to random 1-6
+    let roll = Math.floor(Math.random()*6+1);
+    if (roll = 6) alert("6! you win");
+    else alert("You rolled a " + roll);
+  }
+   
