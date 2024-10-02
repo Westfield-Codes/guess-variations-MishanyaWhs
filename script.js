e5b7cca76f44e1733d31854548e5f8e23ee1ab46
MishanyaWhs <Mykhailo208202@student-schoolsofwestfield.org>
2024-10-02 16:08:57 +0000

    3 done 4 started

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
-/* Simple Guess Program */
+/* Guess Game Program by [Mykhailo]
+ * Adds the ability to play again. 
+ */
 
-// define function guessGame
-function guessGame(){
-  // generate a random integer 1-100
-    var answer = Math.floor(Math.random()*100+1);
+/* newGame is an outer loop giving the user an option 
+ * to play again, using a boolean variable called again.
+ */
+
+// define function newGame
+function newGame(){
+  // create a Boolean variable for again, set to true
+  
+  // loop as long as again is true 
+
+    // run the guessGame function 
+
+    // ask to play again with again set to a confirm
+
+    // close the loop
+
+  // thank the player for playing
+
+  // close the function
+
+
+  
+// create a function called guessGame 
+
   // create guess and set equal to 0 
-    let guess = 0
-  // loop while guess is not equal to answer or to "q"
-    while (guess != answer) 
-    // set guess equal to asking user to "Guess a number, 1-100"
-      
-    // if guess equals "q" display "Quitter!"
 
-    // or if guess equals answer display "Correct!" 
+  // create tries and set equal to 0 
+
+  // create variable answer and generate a random integer 1-100
+
+  // display the integer for testing purposes only
+
+  // loop while guess is not equal to answer
+
+    // set guess equal to asking user to "Guess a number, 1-100"
+
+    // add one to tries
+
+    // if guess equals answer display "Correct in [tries] tries!" 
 
     // or if guess is greater than answer display "Too high!" 
 
     // or if guess is less than answer display "Too low!" 
 
-    // or say "Bad input"
+    // if it's none of those, say "Bad Input!"
 
-  // end the loop
+    // end the loop
 
-// end the function
-}
+  // end the function
