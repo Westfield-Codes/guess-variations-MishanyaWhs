098df021abfbc36531376ccd62f82fbb9163e42d
MishanyaWhs <Mykhailo208202@student-schoolsofwestfield.org>
2024-10-03 16:12:01 +0000

    4 almost finished

--- script.js
+++ script.js
@@ -9,45 +9,44 @@
 // define function newGame
 function newGame(){
   // create a Boolean variable for again, set to true
-  
+  var 
   // loop as long as again is true 
-
+  
     // run the guessGame function 
-
+    
     // ask to play again with again set to a confirm
 
     // close the loop
-
+   
   // thank the player for playing
-
+  if ( guess = answer) alert("Thank you for playing!")
   // close the function
-
+}
 
   
 // create a function called guessGame 
-
+  function guessGame(){
   // create guess and set equal to 0 
-
+    let guess = 0
   // create tries and set equal to 0 
-
+    let tries = 0
   // create variable answer and generate a random integer 1-100
-
-  // display the integer for testing purposes only
-
+    var answer = Math.floor(Math.random()*100+1);
   // loop while guess is not equal to answer
-
+    while (guess != answer);{
     // set guess equal to asking user to "Guess a number, 1-100"
-
+    guess = prompt("Guess a number, 1-100");
     // add one to tries
-
+    tries++;
     // if guess equals answer display "Correct in [tries] tries!" 
-
+    if (guess == answer) alert("Correct in " + tries + " tries!");
     // or if guess is greater than answer display "Too high!" 
-
+    else if (guess > answer) alert("Too high!");
     // or if guess is less than answer display "Too low!" 
-
+    else if (guess < answer) alert("Too low!");
     // if it's none of those, say "Bad Input!"
-
+    else alert("Bad input");
     // end the loop
-
+    }
   // end the function
+  }
